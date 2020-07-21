1dbe8729eb4ad3f3e739b5d39a241af125eae3d4
Teodor Tenchev <teodortenchev@gmail.com>
2020-07-21 20:01:17 +0400

    Limiting the number of origamis shown to configurable number.

--- controllers/origami.js
+++ controllers/origami.js
@@ -2,7 +2,7 @@ const models = require('../models');
 
 module.exports = {
     get: (req, res, next) => {
-        models.Origami.find()
+        models.Origami.find().populate('author', 'username')
             .then((origamies) => res.send(origamies))
             .catch(next);
     },
